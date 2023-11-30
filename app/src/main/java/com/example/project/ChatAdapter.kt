cf2509c76df76b1eb368ccc335ab47a1fafedc33
taemin1971355 <19taemin@hansung.ac.kr>
2023-11-30 21:24:10 +0900

    채팅방 시간 추가

--- app/src/main/java/com/example/project/ChatAdapter.kt
+++ app/src/main/java/com/example/project/ChatAdapter.kt
@@ -1,20 +1,24 @@
 package com.example.project
 
 import android.content.Context
+import android.icu.text.SimpleDateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.QueryDocumentSnapshot
+import java.util.Locale
 
 data class Chat(
     val id: String,
     val text: String,
+    val timestamp: Any
 ) {
     constructor(doc: QueryDocumentSnapshot) : this(
         doc["sender"].toString(),
-        doc["message"].toString()
+        doc["message"].toString(),
+        doc["timestamp"] ?: ""
     )
 }
 
@@ -55,14 +59,21 @@ class ChatAdapter(
 
         val textUser = holder.view.findViewById<TextView>(R.id.textUser)
         val textMessage = holder.view.findViewById<TextView>(R.id.textMessage)
-
+        val textTime = holder.view.findViewById<TextView>(R.id.textTime) // 추가된 부분
         textUser.text = item.id
         textMessage.text = item.text
         // 현재 아이템이 현재 사용자에서 온 것인지 확인
 
 // 메시지가 현재 사용자에서 온 것인지, 다른 사용자에서 온 것인지에 따라 레이아웃 설정
 
-
+        val timestamp = item.timestamp
+        if (timestamp is com.google.firebase.Timestamp) {
+            val date = timestamp.toDate()
+            val formattedTime = SimpleDateFormat("HH:mm", Locale.getDefault()).format(date)
+            textTime.text = formattedTime
+        } else {
+            textTime.text = "Invalid Timestamp"
+        }
 
 
     }
